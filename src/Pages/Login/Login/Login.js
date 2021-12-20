ec7649e806d9fe27b7d99c9dcc4eebfe7c48c2f7
Fariya0909 <95243432+Fariya0909@users.noreply.github.com>
2021-12-20 23:11:20 +0600

    Update Login.js

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -30,8 +30,43 @@ const Login = () => {
     }
     return (
         <div className='container mt-5'>
+        <div className='row row-cols-1 row-cols-md-3 '>
+                <div className="col">
+                    
+                </div>
+                <div className="col">
+                    <div className='w-100 m-auto mt-5 loginFrom'>
+                        <h3 className=' text-center'>Sign In With</h3>
+                        <div>
+                            <form class="row g-3" onSubmit={handleLoginSubmit}>
+                                <div class="col-12">
+                                    <label for="inputEmail4" class="form-label">Email</label>
+                                    <input type="email" className="" name="email" onBlur={handleOnBlur} class="form-control" id="inputEmail4" />
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputPassword4" class="form-label">Password</label>
+                                    <input type="password" name="password" onBlur={handleOnBlur} class="form-control " id="inputPassword4" />
+                                </div>
+                                <div class="col-12 text-center">
+                                    <button type="submit" class="btn btn-dark  text-center">Sign in</button>
+                                </div>
+                            </form>
+                            <p className=' text-center'>Not a Member?<Link to='/register'>Sign up Now</Link> </p>
+                        </div>
+                        <div className=' text-center'>
+                            <button className='btn btn-dark' onClick={handleGoogleSignIn}>Google Sign in</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col">
+                    
+                </div>
+            </div>
             
         </div>
+        
+        
         
     );
 };
